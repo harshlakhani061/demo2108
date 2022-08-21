9bf65854f199ff571aa77e6f9b033e3356644f10
Harsh Lakhani <20it061@charusat.edu.in>
2022-08-21 21:17:32 +0530

    added

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentController {
     @Autowired
-    Repositery repositery;
+    Repositery repositery1;
 
 }
